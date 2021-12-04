d77dc8626f3681c0a580b756b3f1c8e0960522b3
Caroline1972 <caroline.ovall@medieinstitutet.se>
2021-12-05 00:25:15 +0100

    second_commit

--- loginpage-Caroline1972/script.js
+++ loginpage-Caroline1972/script.js
@@ -1,13 +1,26 @@
-console.log("Hej");
-let firstName = document.getElementById('fname');
-let Password = document.getElementById('new-password');
-let btn = document.getElementById('btn');
+let firstName = document.querySelector('fname');
+let Password = document.querySelector('new_password');
+let btn = document.querySelector('btn');
 
-logIn.addEventListener("click", () => {
-  if (firstName === "janne" && Password === "text"){
-      console.log('Welcome!');
-  } else { 
-    console.log("Wrong name or password");
-  }
-  
-})
+//Login Function
+if(window.localStorage){
+localStorage.setItem("user", "janne");
+localStorage.setItem("pass", "test");
+
+let user = localStorage.getItem("user");
+let word = localStorage.getItem("word");
+
+let answer = document.querySelector(".answer");
+
+btn.addEventListener("click",() => {
+if(user === fname.value && word === new_password.value){
+    answer.innerHTML = "Welcome!";
+
+} else {
+    answer.innerHTML ="Username or Password isnt correct. Please try again";
+}
+});
+
+}else{
+    console.log("Not Supported");
+}
